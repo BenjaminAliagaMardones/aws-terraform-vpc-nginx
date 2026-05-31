8ee41fb4197e538d0276abb364619badbfd00a34
BenjaminAliagaMardones <baliaga2025@alu.uct.cl>
2026-05-31 13:23:45 -0400

    Fix indentation for egress CIDR block in firewall.tf

--- firewall.tf
+++ firewall.tf
@@ -31,6 +31,6 @@ resource "aws_security_group" "sg_servidor" {
         from_port = 0
         to_port = 0
         protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
